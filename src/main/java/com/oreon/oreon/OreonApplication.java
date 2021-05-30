bea87f23d909ddbd23fa4e4747fa502b4c36bb74
maurice40 <musabirema@gmail.com>
2021-05-30 12:45:21 +0200

    The take off

---
+++ src/main/java/com/oreon/oreon/OreonApplication.java
@@ -0,0 +1,13 @@
+package com.oreon.oreon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OreonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OreonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oreon/oreon/OreonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oreon.oreon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OreonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
